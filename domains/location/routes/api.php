fe6e724d1f92ac59d3f73032b8ba7026f24c1d4e
Youcef SLIMANI <youcef.slimani@beyn.dz>
2022-12-16 09:03:27 +0100

    Fix bug on city update

--- domains/location/routes/api.php
+++ domains/location/routes/api.php
@@ -16,5 +16,9 @@ Route::prefix('api/cities')
     ->group(function () {
         Route::get('/', Domains\Location\Http\Controllers\CityIndexController::class)->name('cities.index');
         Route::post('/', Domains\Location\Http\Controllers\CityStoreController::class)->name('cities.store');
-        Route::get('{city}', Domains\Location\Http\Controllers\CityShowController::class)->name('cities.show');
+        Route::prefix('{city}')
+            ->group(function() {
+                Route::get('/', Domains\Location\Http\Controllers\CityShowController::class)->name('cities.show');
+                Route::put('/', Domains\Location\Http\Controllers\CityUpdateController::class)->name('cities.update');
+            });
 });
