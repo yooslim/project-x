5bc9083c96167f676b630396773b8587c96c4a50
Youcef SLIMANI <youcef.slimani@beyn.dz>
2022-12-16 05:06:14 +0100

    Better approach to fix factories bug regarding DDD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,10 @@
 
 namespace App\Providers;
 
-use App\Utils\DomainDrivenFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Foundation\AliasLoader;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +16,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $loader = AliasLoader::getInstance();
-        $loader->alias(Factory::class, DomainDrivenFactory::class);
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            // Get the domain name
+            $domain = Str::betweenFirst($modelName, '\\', '\\');
+
+            // Get the model name
+            $model = Str::afterLast($modelName, '\\');
+
+            // We can also customise where our factories live too if we want:
+            $namespace = 'Domains\\' . $domain . '\\Database\\Factories\\';
+
+            // Finally we'll build up the full class path where
+            // Laravel will find our model factory
+            return $namespace.$model.'Factory';
+        });
+
+        Factory::guessModelNamesUsing(function (Factory $factory) {
+            // Get the domain name
+            $domain = Str::betweenFirst(get_class($factory), '\\', '\\');
+
+            // Get the model name
+            $model = Str::beforeLast(Str::afterLast(get_class($factory), '\\'), 'Factory');
+
+            return 'Domains\\' . $domain . '\\Models\\' . $model;
+        });
     }
 
     /**
